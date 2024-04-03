b8c0928652c7a4eeb1076d934097b1008ce0bc01
Karol Marchewka <karol.marchewka@outlook.com>
2024-04-03 21:58:30 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bm-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ngrx-signals-bank-messages';
+}
